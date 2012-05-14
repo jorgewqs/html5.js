d41a7d525b780271dffa3cdc6360125619acf991
John-David Dalton <john.david.dalton@gmail.com>
2012-05-14 19:03:30 -0400

    Cleanup build.js, post-compile.js, and minify.js.

--- build.js
+++ build.js
@@ -7,23 +7,46 @@
       path = require('path');
 
   /** The minify module */
-  var Minify = require(path.join(__dirname, 'build', 'minify'));
+  var minify = require(path.join(__dirname, 'build', 'minify'));
+
+  /** A list of print related functions */
+  var printFuncs = [
+    'addPrintSheet', 'addPrintWrappers', 'createPrintWrapper',
+    'removePrintWrappers', 'setPrintSupport', 'unsetPrintSupport'
+  ];
 
   /** The html5.js source */
   var source = fs.readFileSync(path.join(__dirname, 'html5.js'), 'utf8');
 
   /*--------------------------------------------------------------------------*/
 
+  /**
+   * Removes the `funcName` function declaration from the `source`.
+   *
+   * @private
+   * @param {String} source The source to process.
+   * @param {String} funcName The name of the function to remove.
+   * @returns {String} Returns the source with the function removed.
+   */
+  function removeFunction(source, funcName) {
+    return source.replace(RegExp(
+      // match multi-line comment block (could be on a single line)
+      '\\n +/\\*[^*]*\\*+(?:[^/][^*]*\\*+)*/\\n' +
+      // match a function declaration
+      '( +)function ' + funcName + '\\b[\\s\\S]+?\\n\\1}\\n'
+    ), '');
+  }
+
+  /*--------------------------------------------------------------------------*/
+
   // begin the minification process
-  new Minify(source, 'html5.min', function(result) {
+  minify(source, 'html5.min', function(result) {
     // create a build of html5.js without HTML5 print support
     var noprint = source;
 
     // remove print related functions
-    ['addPrintSheet', 'addPrintWrappers', 'createPrintWrapper',
-     'removePrintWrappers', 'setPrintSupport', 'unsetPrintSupport'
-    ].forEach(function(funcName) {
-        noprint = noprint.replace(RegExp('\n +/\\*[^*]*\\*+(?:[^/][^*]*\\*+)*/\\n( +)function ' + funcName + '[\\s\\S]+?\\n\\1};?\n'), '');
+    printFuncs.forEach(function(funcName) {
+      noprint = removeFunction(noprint, funcName);
     });
 
     // remove "print" options
@@ -32,11 +55,11 @@
     // remove `html5Printing` support test
     noprint = noprint.replace(/\n +\/\*[^*]*\*+(?:[^\/][^*]*\*+)*\/\n( +)support.html5Printing *=[\s\S]+?\n\1\);?\n/, '');
 
-    // remove related if-statements
+    // remove `html5Printing` related if-statements
     noprint = noprint.replace(/\n +if *\(!support\.html5Printing[^}]+}/, '');
 
     // write files
-    new Minify(noprint, 'html5.noprint.min', function(noprintResult) {
+    minify(noprint, 'html5.noprint.min', function(noprintResult) {
       fs.writeFileSync(path.join(__dirname, 'html5.min.js'), result);
       fs.writeFileSync(path.join(__dirname, 'html5.noprint.js'), noprint);
       fs.writeFileSync(path.join(__dirname, 'html5.noprint.min.js'), noprintResult);
